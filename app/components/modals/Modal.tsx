21917ccfbdcccf37ee341759abd384f1234d8448
Blake Zajac <contact@blakezajac.com>
2023-08-19 21:03:10 +1000

    Fix modal component markup

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -23,8 +23,8 @@ const Modal: React.FC<ModalProps> = ({
   onSubmit,
   title,
   body,
-  footer,
   actionLabel,
+  footer,
   disabled,
   secondaryAction,
   secondaryActionLabel,
@@ -41,11 +41,10 @@ const Modal: React.FC<ModalProps> = ({
     }
 
     setShowModal(false);
-
     setTimeout(() => {
       onClose();
     }, 300);
-  }, [disabled, onClose]);
+  }, [onClose, disabled]);
 
   const handleSubmit = useCallback(() => {
     if (disabled) {
@@ -53,7 +52,7 @@ const Modal: React.FC<ModalProps> = ({
     }
 
     onSubmit();
-  }, [disabled, onSubmit]);
+  }, [onSubmit, disabled]);
 
   const handleSecondaryAction = useCallback(() => {
     if (disabled || !secondaryAction) {
@@ -61,7 +60,7 @@ const Modal: React.FC<ModalProps> = ({
     }
 
     secondaryAction();
-  }, [disabled, secondaryAction]);
+  }, [secondaryAction, disabled]);
 
   if (!isOpen) {
     return null;
@@ -70,22 +69,24 @@ const Modal: React.FC<ModalProps> = ({
   return (
     <>
       <div
-        className="flex items-center justify-center overflow-y-auto
-      fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/50"
+        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed 
+      inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70"
       >
         <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
           {/* Content */}
           <div
             className={`translate duration-300 h-full ${
-              showModal ? "translate-y-0" : "translate-y-full"
-            } ${showModal ? "opacity-100" : "opacity-0"}`}
+              showModal
+                ? "translate-y-0 opacity-100"
+                : "translate-y-full opacity-0"
+            }`}
           >
             <div
-              className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg
-            shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none"
+              className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex
+            flex-col w-full bg-white outline-none focus:outline-none"
             >
               {/* Header */}
-              <div className="flex items-center p-6 rounded-t justify-center relative border-t">
+              <div className="flex items-center p-6 rounded-t justify-center relative border-b-[1px]">
                 <button
                   className="p-1 border-0 hover:opacity-70 transition absolute left-9"
                   onClick={handleClose}
@@ -95,29 +96,30 @@ const Modal: React.FC<ModalProps> = ({
 
                 <div className="text-lg font-semibold">{title}</div>
               </div>
-            </div>
 
-            {/* Body */}
-            <div className="relative p-6 flex-auto">{body}</div>
-          </div>
+              {/* Body */}
+              <div className="relative p-6 flex-auto">{body}</div>
 
-          {/* Footer */}
-          <div className="flex flex-col gap-2 p-6">
-            <div className="flex items-center gap-4 w-full">
-              {secondaryAction && secondaryActionLabel && (
-                <Button
-                  outline
-                  disabled={disabled}
-                  label={secondaryActionLabel}
-                  onClick={handleSecondaryAction}
-                />
-              )}
+              {/* Footer */}
+              <div className="flex flex-col gap-2 p-6">
+                <div className="flex flex-row items-center gap-4 w-full">
+                  {secondaryAction && secondaryActionLabel && (
+                    <Button
+                      disabled={disabled}
+                      label={secondaryActionLabel}
+                      onClick={handleSecondaryAction}
+                      outline
+                    />
+                  )}
 
-              <Button
-                disabled={disabled}
-                label={actionLabel}
-                onClick={handleSubmit}
-              />
+                  <Button
+                    disabled={disabled}
+                    label={actionLabel}
+                    onClick={handleSubmit}
+                  />
+                </div>
+                {footer}
+              </div>
             </div>
           </div>
         </div>
