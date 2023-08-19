12c73fbae38c5e5b1c0256d9c4e90a9028f5ae57
Blake Zajac <contact@blakezajac.com>
2023-08-20 00:32:37 +1000

    Pass currentUser into navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import { User } from "@prisma/client";
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 
-interface NavbarProps {}
+interface NavbarProps {
+  currentUser?: User;
+}
 
-const Navbar: React.FC<NavbarProps> = ({}) => {
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   return (
     <header className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b">
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import ClientOnly from "./components/ClientOnly";
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -14,11 +15,13 @@ export const metadata: Metadata = {
   description: "Airbnb clone developed by Blake Zajac",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
       <body className={font.className}>
@@ -28,7 +31,7 @@ export default function RootLayout({
           <LoginModal />
           <RegisterModal />
 
-          <Navbar />
+          <Navbar currentUser={currentUser} />
           {children}
           {/* <Footer /> */}
         </ClientOnly>
