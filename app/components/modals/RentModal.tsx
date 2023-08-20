27811ea48665535506c99ae3d2e0b68b07fa8d2e
Blake Zajac <contact@blakezajac.com>
2023-08-20 13:17:01 +1000

    Create ImageUpload

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -10,6 +10,7 @@ import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
 
 interface RentModalProps {}
 
@@ -165,6 +166,19 @@ const RentModal: React.FC<RentModalProps> = ({}) => {
           value={bathroomCount}
           onChange={(value) => setCustomValue("bathroomCount", value)}
         />
+      </div>
+    );
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Add a photo of your place!"
+          subtitle="Show guests what your place looks like."
+        />
+
+        <ImageUpload />
       </div>
     );
   }
