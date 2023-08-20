5d5ffa07aa69367964cd8ebdf161d995611aa54e
Blake Zajac <contact@blakezajac.com>
2023-08-20 18:14:13 +1000

    Set up SafeListing type

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,18 +1,18 @@
 "use client";
 
-import { Listing, Reservation } from "@prisma/client";
+import { Reservation } from "@prisma/client";
 import { useRouter } from "next/navigation";
 import React, { useCallback, useMemo } from "react";
 import { format } from "date-fns";
 
-import { SafeUser } from "@/app/types";
+import { SafeListing, SafeUser } from "@/app/types";
 import useCountries from "@/app/hooks/useCountries";
 import Image from "next/image";
 import HeartButton from "../common/HeartButton";
 import Button from "../common/Button";
 
 interface ListingCardProps {
-  data: Listing;
+  data: SafeListing;
   reservation?: Reservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
