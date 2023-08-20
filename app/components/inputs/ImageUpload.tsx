0d603a48a8e1db2f81084d41ff153b4ed3f869d5
Blake Zajac <contact@blakezajac.com>
2023-08-20 13:51:00 +1000

    Optimise Imageupload preset

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -9,6 +9,8 @@ declare global {
   var cloudinary: any;
 }
 
+const uploadPreset = "pgc9ehd5";
+
 interface ImageUploadProps {
   onChange: (value: string) => void;
   value: string;
@@ -25,7 +27,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ onChange, value }) => {
   return (
     <CldUploadWidget
       onUpload={handleUpload}
-      uploadPreset=""
+      uploadPreset={uploadPreset}
       options={{
         maxFiles: 1,
       }}
@@ -34,10 +36,23 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ onChange, value }) => {
         return (
           <div
             onClick={() => open?.()}
-            className="relative cursor-pointer hover:opacity-70 transition border-dashed border-2
-            p-20 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600"
+            className="relative cursor-pointer hover:opacity-70 transition border-dashed border-2 p-20 border-neutral-300
+            flex flex-col justify-center items-center gap-4 text-neutral-600"
           >
-            <TbPhotoPlus />
+            <TbPhotoPlus size={48} />
+
+            <div className="font-semibold">Click to upload</div>
+
+            {value && (
+              <div className="absolute inset-0 w-full h-full">
+                <Image
+                  fill
+                  style={{ objectFit: "cover" }}
+                  src={value}
+                  alt="House"
+                />
+              </div>
+            )}
           </div>
         );
       }}
