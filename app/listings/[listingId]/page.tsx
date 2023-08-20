c0efdd339f14cb9539361947370ca338809f8c32
Blake Zajac <contact@blakezajac.com>
2023-08-20 18:18:07 +1000

    Create individual listing page

---
+++ app/listings/[listingId]/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+interface ListingPageProps {}
+
+const ListingPage: React.FC<ListingPageProps> = () => {
+  return <div>Individual Listing Page</div>;
+};
+
+export default ListingPage;
