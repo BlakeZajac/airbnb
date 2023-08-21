fa814e3f8da27833da25f3db0dbf69f6ae1868c0
Blake Zajac <contact@blakezajac.com>
2023-08-21 23:06:52 +1000

    Set up Reservations page

---
+++ app/reservations/page.tsx
@@ -0,0 +1,39 @@
+import ClientOnly from "../components/common/ClientOnly";
+import EmptyState from "../components/common/EmptyState";
+
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservations";
+
+const ReservationsPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="Unauthorised"
+          subtitle="Please login to view reservations."
+        />
+      </ClientOnly>
+    );
+  }
+
+  const reservations = await getReservations({
+    authorId: currentUser.id,
+  });
+
+  if (reservations.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No reservations found"
+          subtitle="Looks like you have no reservations on your properties."
+        />
+      </ClientOnly>
+    );
+  }
+
+  return <div></div>;
+};
+
+export default ReservationsPage;
