0092eee4b544a9c8cd2fa4737c46eaba38351e88
Blake Zajac <contact@blakezajac.com>
2023-08-22 22:39:42 +1000

    Update getListings query

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,16 +2,84 @@ import prisma from "@/app/libs/prismadb";
 
 export interface IListingParams {
   userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
 
 export default async function getListings(params: IListingParams) {
   try {
-    const { userId } = params;
+    const {
+      userId,
+      guestCount,
+      roomCount,
+      bathroomCount,
+      startDate,
+      endDate,
+      locationValue,
+      category,
+    } = params;
 
     let query: any = {};
 
     if (userId) {
       query.userId = userId;
+    }
+
+    if (category) {
+      query.category = category;
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        // Query listings from room count the user sent, filtering everything out that's greater than
+        gte: +roomCount,
+      };
+    }
+
+    if (guestCount) {
+      // Query listings from guest count the user sent, filtering everything out that's greater than
+      query.guestCount = {
+        gte: +guestCount,
+      };
+    }
+
+    if (bathroomCount) {
+      // Query listings from bathroom count the user sent, filtering everything out that's greater than
+      query.bathroomCount = {
+        gte: +bathroomCount,
+      };
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    // Filter out all reservations that have a reservation in the users selected date range
+    // If there is a signle day in the reservation date range, we filter out the listing from
+    // showing in the search results as the user can not make a reservation within that date range.
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate },
+              },
+
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate },
+              },
+            ],
+          },
+        },
+      };
     }
 
     const listings = await prisma.listing.findMany({
